e7e618d596f6af7d6db09d2786172133217f386a
Bram <warsocket@gmail.com>
2018-04-18 21:18:21 +0200

    Added smtp and submission password engine Added crude possibilty to set your domain name (used by smtp engine)

--- honeypass.go
+++ honeypass.go
@@ -1,16 +1,21 @@
 package main
 import "net"
-// import "os"
+import "os"
+import "fmt"
 import "time"
 import "bufio"
 import "regexp"
-// import "strings"
+import "log"
+import "strings"
 import "encoding/base64"
 
-func TcpHandler(listenaddress string){
+
+var hostname string = "example.com"
+
+func TcpHandler(listenaddress string, handleFunc func(net.Conn)){
 	listen, err := net.Listen("tcp", listenaddress)
 	if err != nil {
-		print("Error Listening\n")
+		log.Fatal("Error Listening")
 		return
 	}
 
@@ -19,27 +24,29 @@ func TcpHandler(listenaddress string){
 	for{
 		conn ,_ := listen.Accept()
 		if err == nil{
-			handle(conn)	
+			handleFunc(conn)	
 		}else{
-			print("Error Accpeting\n")
+			log.Println("Error Acepting")
 		}
 		
 	}
 }
 
-
-
 func main(){
-	go TcpHandler("0.0.0.0:80")
+	if len(os.Args) > 1 {
+		hostname = os.Args[1]
+	}
+
+	go TcpHandler("0.0.0.0:80", handleHttp)
+	go TcpHandler("0.0.0.0:25", handleSmtp)
+	go TcpHandler("0.0.0.0:587", handleSmtp) //submission, but the handler works for smtp and submission
 
 	for{
 		time.Sleep(1)
 	}
 }
 
-
-
-func handle(conn net.Conn){
+func handleHttp(conn net.Conn){
 	reader := bufio.NewReader(conn)
 	writer := bufio.NewWriter(conn)
 
@@ -62,11 +69,83 @@ func handle(conn net.Conn){
 
 	writer.WriteString(`HTTP/1.1 401 Unauthorized
 Server: Honeypot
-WWW-Authenticate: Basic realm=""
+WWW-Authenticate: Basic realm="Hive"
 Connection: Close
 
 `)
 	writer.Flush()
-
 	conn.Close()
+}
+
+func handleSmtp(conn net.Conn){
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	writer := bufio.NewWriter(conn)
+
+	fmt.Fprintf(writer, "220 %s ESMTP Honeypot\r\n", hostname)
+	writer.Flush()
+
+	// nuke := func(){
+	// 	writer.WriteString("550 Bzzz, *stings*\r\n")
+	// 	writer.Flush()
+	// 	conn.Close()
+	// 	return
+	// }
+
+	// Stateless command processing
+	// We allow way more abuse/erros then the RFC's, we dont care, we need passwords.
+	for{
+		fullcmd, _, err := reader.ReadLine()
+		if err != nil {break}
+
+		if len(fullcmd) < 4 {
+			fmt.Fprintf(writer, "502 5.5.2 Error: command not recognized\r\n")
+			writer.Flush()
+			continue
+		}
+		cmd := strings.ToUpper(string(fullcmd[:4]))
+
+		if cmd == "HELO"{
+			fmt.Fprintf(writer, "250 %s\r\n", hostname)
+
+		} else if cmd == "EHLO" {
+			fmt.Fprintf(writer, "250-%s\r\n", hostname)
+			fmt.Fprintf(writer, "250 AUTH PLAIN\r\n")
+
+		} else if cmd == "MAIL" {
+			fmt.Fprintf(writer, "250 2.1.0 Ok\r\n")
+
+		} else if cmd == "RCPT" {
+			fmt.Fprintf(writer, "554 5.7.1 Error: Relay access denied\r\n")
+
+		} else if cmd == "AUTH" {
+			matched, _ := regexp.MatchString("^AUTH PLAIN\\s*$", strings.ToUpper(string(fullcmd)))
+			if matched { //bit of state needed here
+				fmt.Fprintf(writer, "334\r\n")
+				writer.Flush()
+
+				userpass, _, err := reader.ReadLine()
+				if err != nil {break}
+
+				decoded, err := base64.StdEncoding.DecodeString(string(userpass))
+				if err == nil{
+					userpassPlain := strings.Replace(string(decoded),"\x00",":",-1)
+					if len(userpassPlain) > 0{
+						print( userpassPlain[1:] + "\n")
+					}
+				}
+				fmt.Fprintf(writer, "535 5.7.8 Error: authentication failed\r\n")
+
+			} else {
+				fmt.Fprintf(writer, "535 5.7.8 Error: authentication failed: Invalid authentication mechanism\r\n")
+			}	
+
+		} else {
+			fmt.Fprintf(writer, "502 5.5.2 Error: command not recognized\r\n")
+		}
+
+		writer.Flush()
+	}
+	
+
 }
